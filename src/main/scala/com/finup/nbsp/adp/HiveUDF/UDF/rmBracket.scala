65de307d04933c87b6e55bded46767aad023f828
neighborhood <john_liu@laplace-work.local>
2017-11-22 18:20:40 +0800

    fix 异常右括号

--- src/main/scala/com/finup/nbsp/adp/HiveUDF/UDF/rmBracket.scala
+++ src/main/scala/com/finup/nbsp/adp/HiveUDF/UDF/rmBracket.scala
@@ -2,7 +2,7 @@ package com.finup.nbsp.adp.HiveUDF.UDF
 
 import org.apache.hadoop.hive.ql.exec.UDF
 import org.apache.hadoop.io.Text
-
+import scala.util.control._
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
@@ -23,19 +23,28 @@ class rmBracket extends UDF {
     val theBrackets = allByteArray
       .filter(x => brackets.contains(x._2))
     //初始化stack
-    val stack = new mutable.Stack[(Int, Byte)]
+    val stack    = new mutable.Stack[(Int, Byte)]
     //记录被排除区间
-    val lb = ListBuffer.empty[(Int, Int)]
-    //利用栈匹配括号，被排除的区间记录在lb中
+    val lb       = ListBuffer.empty[(Int, Int)]
+    //break是为了当遇到异常右括号无法入栈匹配时，break循环，直接进入判定
+    val loop     = new Breaks
+    loop.breakable{
+      //利用栈匹配括号，被排除的区间记录在lb中
     for (bracket <- theBrackets) {
       if (leftBrackets.contains(bracket._2)) {
         stack.push(bracket);
       } else {
+        if(stack.isEmpty){
+          stack.push(bracket)
+          loop.break()
+        }else{
         val left = stack.pop()
         val right = bracket
         val closedInterval = (left._1, right._1)
         lb.append(closedInterval)
+        }
       }
+    }
     }
     //
     val filterSet = lb
